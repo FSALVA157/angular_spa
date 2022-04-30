02261b8d64f69d4758f9becc1e70db033204ff32
xxavierargentino <xxavierargentino@hotmail.com>
2022-04-30 00:34:22 -0300

    navbar y rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/shared/navbar/navbar.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { AboutComponent } from './componentes/about/about.component';
+import { HeroesComponent } from './componentes/heroes/heroes.component';
+import { APP_ROUTING } from './app.routes';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,27 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { HomeComponent } from './componentes/home/home.component';
+import { HeroesComponent } from './componentes/heroes/heroes.component';
+import { AboutComponent } from './componentes/about/about.component';
+
+// import { Name2Component } from './';
+// import { Name3Component } from './';
+// import { Name4Component } from './';
+// import { PageNotFoundComponent } from './';
+
+const ROUTES: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'heroes', component: HeroesComponent },
+    { path: 'about', component: AboutComponent },
+    // { path: 'path4', component: Name4Component },
+    // { path: '**', component: PageNotFoundComponent },
+
+    //{ path: 'path/:routeParam', component: MyComponent },
+    //{ path: 'staticPath', component: ... },
+    //{ path: '**', component: ... },
+    { path: '**', pathMatch: 'full', redirectTo: 'home'},
+    //{ path: ..., component: ..., data: { message: 'Custom' }
+];
+
+export const APP_ROUTING = RouterModule.forRoot(ROUTES,{useHash: true});
